09cacf2b10864bb062112538d1ce72e23f6cb252
Lychar85 <63816497+Lychar85@users.noreply.github.com>
2021-02-02 18:25:32 +0100

    components countries/card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
+import Coutries from "../components/Countries"
+import Logo from "../components/Logo"
 import Navigation from "../components/Navigation"
 
 const Home = () =>{
     return(
         <div className="home">
             <Navigation />
+            <Logo/>
+            <Coutries/>
             <h1>Accueil</h1>
         </div>
     )
